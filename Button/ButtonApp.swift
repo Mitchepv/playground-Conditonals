6f32824ae2a67b56aeb25908e8f7c99f62093e56
Mitchepv <143571339+Mitchepv@users.noreply.github.com>
2026-01-14 11:43:03 -0500

    Initial Commit

---
+++ Button/ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonApp.swift
+//  Button
+//
+//  Created by Nia Mitchell on 1/14/26.
+//
+
+import SwiftUI
+
+@main
+struct ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
